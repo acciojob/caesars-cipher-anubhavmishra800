5083d5cb9dd515f73b18adf4142163c81c19ad8f
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-05-21 14:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,18 +10,21 @@ const lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	for(let i = 0 ; i < encodedStr.length ; i++){
-        if(lookup[encodedStr[i]] === undefined){
-            decodedArr += encodedStr[i];
-        }
-        else{
-            decodedArr += lookup[encodedStr[i]];
-        }
+function rot13(str) {
+  let decodedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) {
+      // Rotate the letter by 13 places
+      charCode = ((charCode - 65 + 13) % 26) + 65;
     }
-  return decodedArr ;//return decodedArr
+
+    decodedStr += String.fromCharCode(charCode);
+  }
+
+  return decodedStr;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
